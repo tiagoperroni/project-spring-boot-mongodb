37d9a68e296772a877a35b70bdb8f6c6d1792afc
tiagoperroni <tiagoperroni86@gmail.com>
2021-06-24 16:23:50 -0300

    first commit project created

---
+++ src/main/java/com/tiagoperroni/projetofullstack/ProjectspringbootMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.tiagoperroni.projetofullstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectspringbootMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectspringbootMongodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tiagoperroni/projetofullstack/ProjectspringbootMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiagoperroni.projetofullstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectspringbootMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
